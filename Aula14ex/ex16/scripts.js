c99a1a5ec263580ad9acdce42c55b0a9422e949c
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2022-07-31 22:49:49 -0300

    2022/07/31 - Correção de erros do ex16. Substituição de string.lenght que não funciona.

--- Aula14ex/ex16/scripts.js
+++ Aula14ex/ex16/scripts.js
@@ -3,8 +3,10 @@ function contar(){
     let fim = window.document.getElementById('txtf')
     let passo = window.document.getElementById('txtp')
     let res = window.document.getElementById('res')
-
-    if(ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+   
+    //if(ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+    //string.value.lenght não funciona!!!
+    if(ini.value == '' || fim.value == '' || passo.value == ''){
         window.alert('Erro! Preencha todos os campos!')
     }
     else{
@@ -16,12 +18,21 @@ function contar(){
             window.alert("Passo inválido! Considerando o passo igual a 1!")
             p = 1
         }
-        if(i > f){
-            p = p*(-1)
+        else if(p < 0){ //Se o passo for negativo, é convertido para positivo.
+            window.alert("O passo negativo é considerado em módulo mesmo se o inicial for maior que o final.")
+            p = p * (-1)
         }
-        for(let c = i; c <= f; c += p){
-            res.innerHTML += ` ${c} \u(1F449)`
+        if(i < f){ //Contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} ->`
+            }    
         }
-        res.innerHTML += `\u(1F3C1)`
+        else{ //Contagem regressiva
+            for(let c = i; c >= f; c = c - p){
+                res.innerHTML += ` ${c} ->`
+            }
+        }
+        
+        res.innerHTML += `FIM`
     }
 }
