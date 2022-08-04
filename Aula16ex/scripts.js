14b0c9297614a1c5eeaafa475b59e744b69d836e
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2022-08-03 22:41:58 -0300

    Exercício resolvido!

--- Aula16ex/scripts.js
+++ Aula16ex/scripts.js
@@ -4,8 +4,9 @@ let res = window.document.querySelector('div#res')
 let valores = []
 
 
+
 function isNumero(n){
-    if((Number(n) >= 1) && (Number(n) <=100)){
+    if((n >= 1) && (n <=100)){
         return true
     }
     else{
@@ -14,7 +15,7 @@ function isNumero(n){
 }
 
 function inLista(n, l){
-    if(l.indexOf(Number(n) == -1)){
+    if(l.indexOf(n) == -1){
         return false
     }
     else{
@@ -23,10 +24,36 @@ function inLista(n, l){
 }
 
 function adicionar(){
-    if(isNumero(num.value) && !inLista(num.value, valores)){
-        
+    if(isNumero(Number(num.value)) && !inLista(Number(num.value), valores)){
+        valores.push(Number(num.value))
+        let item = window.document.createElement('option')
+        item.text = `${num.value}`
+        //item.value = `lista${num.value}`
+        fseltab.appendChild(item)
+
     }
     else{
         window.alert("Valor fora dos limites entre 1-100 ou repetido!")
     }
+}
+
+function finalizar(){
+    res.innerHTML = ''
+    let maior = -1
+    let menor = 101
+    let soma = 0
+    
+    for(var i in valores){
+        if(valores[i] > maior){
+            maior = valores[i]
+        }
+        if(valores[i] < menor){
+            menor = valores[i]
+        }
+        soma = soma + valores[i]
+    }
+    res.innerHTML += `O maior número é ${maior}<br>`
+    res.innerHTML += `O menor número é ${menor}<br>`
+    res.innerHTML += `O somatório é  ${soma}<br>`
+    res.innerHTML += `A média é  ${soma/valores.length}`
 }
